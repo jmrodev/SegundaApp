591e9e5268392dc03dc8926aaab013a2e75b6efd
jmrodev <chello1975@gmail.com>
2022-11-26 20:25:17 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'SegundaApp';
 }
+articulos=[
+{codigo:1,descripcion:'Articulo 1',precio:100},
+{codigo:2,descripcion:'Articulo 2',precio:200},
+{codigo:3,descripcion:'Articulo 3',precio:300},
+{codigo:4,descripcion:'Articulo 4',precio:400},
+{codigo:5,descripcion:'Articulo 5',precio:500},
+]
